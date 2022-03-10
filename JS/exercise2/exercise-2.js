06b5472bd2f1899e17c82aac2e8a916e81f2c6f4
Antonio Valerio Calo <gasthaus.roessle@outlook.de>
2022-03-10 17:15:43 +0100

    add/exercise2

---
+++ JS/exercise2/exercise-2.js
@@ -0,0 +1,15 @@
+function canPlay() {
+  
+  const PERSON_NAME = 'Paul' ;
+ 
+  if (true) {
+   let personName = 'George'; // Argh! Voglio che personName sia una variabile di sola lettura
+   personName += ' plays football';
+   console.log(personName);  
+  }
+  
+
+ 
+}
+
+canPlay();
