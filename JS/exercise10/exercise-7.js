f5d59977f1d9bac1a8fc10b0e042195080e90a3c
Antonio Valerio Calo <gasthaus.roessle@outlook.de>
2022-03-23 12:53:21 +0100

    added/§exercise11

--- JS/exercise10/exercise-7.js
+++ JS/exercise10/exercise-7.js
@@ -1,14 +1,12 @@
 function sumUntil(maxValue) {
   // ...
-    let sumValue=[];
-    let valuePlus=0;
-    for(let value=1; value<=maxValue; value++){
-      valuePlus+= value;
-      sumValue.push("+" + value);
-      }
-        
-    
-  
-return   sumValue + "=" + valuePlus  ;
+  let sumValue = [];
+  let valuePlus = 0;
+  for (let value = 1; value <= maxValue; value++) {
+    valuePlus += value;
+    sumValue.push("+" + value);
   }
+
+  return sumValue + "=" + valuePlus;
+}
 console.log(sumUntil(5));
---
+++ JS/exercise11/exercise-1.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => a + b;
+
+const log = (value) => value;
+
+console.log(log(sum(2, 5)));
